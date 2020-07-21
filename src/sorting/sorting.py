0e821c04fcb8200b1b30872a7f11401ecb8a1717
Ian McElroy <mcelroyian@gmail.com>
2020-07-21 18:11:41 -0500

    wip

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -3,9 +3,19 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    # Your code here
-
-
+    i_of_A = 0
+    i_of_B = 0
+    #As long as there are still items in one of the arrays
+    while i_of_A < len(arrA) and i_of_B < len(arrB):
+        #Add from Array A if we reached the end of Array B or 
+        #the current item in Array A is smaller
+        if i_of_B == len(arrB) or arrA[i_of_A] <= arrB[i_of_B]:
+            merged_arr.append(arrA[i_of_A])
+            i_of_A += 1
+        #Otherwise add from Array B
+        else:
+            merged_arr.append(arrB[i_of_B])
+            i_of_B += 1
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
